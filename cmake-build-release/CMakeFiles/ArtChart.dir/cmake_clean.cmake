ff052cc394f10cb4ec8539aa0b2e24d0d145cfc5
RoboZero <68201467+RoboZero@users.noreply.github.com>
2022-04-12 00:23:42 -0400

    Commented out Catch2 usage, couldn't figure out how to make it work rn

---
+++ cmake-build-release/CMakeFiles/ArtChart.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "ArtChart.exe"
+  "ArtChart.exe.manifest"
+  "ArtChart.pdb"
+  "CMakeFiles/ArtChart.dir/main.cpp.obj"
+  "CMakeFiles/ArtChart.dir/main.cpp.obj.d"
+  "libArtChart.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ArtChart.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
