bfd35750bb86acd72f637ca6dd379fa69a57e378
Emmett-Kogan <emmett.kogan@gmail.com>
2022-04-14 23:49:32 -0400

    Updated with start to parseing.
    Some lines in the dataset have mismatching quotation marks. This makes them extremely difficult to parse correctly. Since we have six times as many points as we need if everyone is ok with it I'd like to delete these lines (given they aren't the bulk of the data set).

--- main.cpp
+++ main.cpp
@@ -1,7 +1,96 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include "Piece.h"
 
-int main()
+std::vector<Piece> parse_dataset(); // Final version of the parser, does the entire dataset
+std::vector<Piece> parse_dataset(int count); // temp param = lines to get, so we arent reading all 600,000 lines each trial
+
+int main(int argc, char **argv)
 {
-    std::cout << "Hello, World!" << std::endl;
+	std::vector<Piece> gallery = parse_dataset(std::stoi(argv[1]));
     return 0;
+}
+
+std::vector<Piece> parse_dataset() 
+{
+	std::vector<Piece> gallery;
+	// TODO
+	return gallery;
+}
+
+std::vector<Piece> parse_dataset(int count) 
+{
+	int i, iter, index;
+	std::string in, temp, piece, period, artist, medium;
+	std::ifstream dataset;
+	std::vector<Piece> gallery;
+	
+	dataset.open("MetObjects.txt");
+	getline(dataset, in); // get rid of first line which is information about the following rows of data
+	
+	
+	for (i = 0; i < count; i++)
+	{
+		std::getline(dataset, in);
+		std::cout << "Line " << i << ":\t" << in << std::endl;
+		iter = 1;
+		while (in.length() > 0)
+		{
+			index = 0;
+			while (in.at(index) != ',' && index < in.length()-1) 
+			{
+				if (in.at(index) == '\"') // need to look for next quote then comma
+				{
+					index++;
+					while (in.at(index) != '\"')
+						index++;
+					index += 1;
+					break;
+				}
+				index++;
+			}
+			
+			if (in.length()-1 != index) 
+				temp = in.substr(0,index);
+			else
+				temp = in;
+			if (i == 36) std::cout << temp << std::endl;
+			in = in.substr(index+1, in.length()-index);
+			//std::cout << "iteration" << iter << ":\t" << temp << "\t" << in << std::endl;
+			
+			if (temp == "") temp = "unknown";
+			switch (iter)
+			{
+				case 10:
+					std::cout << "Piece name: " << temp << std::endl;
+					piece = temp;
+					break;
+				case 12:
+					std::cout << "Period: " << temp << std::endl;
+					period = temp;
+					break;
+				case 19:
+					std::cout << "Artist name: " << temp << std::endl;
+					artist = temp;
+					break;
+				case 32:
+					std::cout << "Medium: " << temp << std::endl;
+					medium = temp;
+					break;
+				default:
+					break;
+			}
+			
+			if (iter == 32) break;
+			iter++;
+		}
+		// check if any unknown, continue
+		gallery.push_back(Piece(piece, period, artist, medium));
+		std::cout << std::endl;
+		
+	}		
+	dataset.close();
+	return gallery;
 }
