e008bcd3886ec9e3459990d6c98dfe2f354313eb
Roland <rolando.arias@ufl.edu>
2022-04-17 15:15:28 -0400

    radix sort

--- RadixSort.h
+++ RadixSort.h
@@ -2,7 +2,7 @@
 // Created by Roland on 4/17/2022.
 //
 
-#include "RadixSort.h"
+#include "Piece.h"
 
 //https://stackoverflow.com/a/1505791
 int pow(int x, unsigned int p){
@@ -14,8 +14,7 @@ int pow(int x, unsigned int p){
     else return x * tmp * tmp;
 }
 
-
-void clearBuckets(std::vector<std::vector<int>>& bucket){
+void clearBuckets(std::vector<std::vector<Piece>>& bucket){
     for (auto& b: bucket){
         b.clear();
     }
@@ -25,37 +24,37 @@ std::vector<Piece> sort(std::vector<Piece>& list, int nBuckets){
     int max = INT_MIN;
     int min = INT_MAX;
 
-    for (auto v: list){
-        if (v < min){
-            min = v;
+    for (auto piece: list){
+        if (piece < min){
+            min = piece;
         }
-        if (v > max){
-            max = v;
+        if (piece > max){
+            max = piece;
         }
     }
 
-    std::vector<std::vector<int>> buckets(nBuckets); buckets[0] = list;
-    std::vector<std::vector<int>> temp(nBuckets);
+    std::vector<std::vector<Piece>> buckets(nBuckets); buckets[0] = list;
+    std::vector<std::vector<Piece>> temp(nBuckets);
 
     for (int i = 0;; i++){
         int nPowI = pow(nBuckets, i);
         if (nPowI > max-min) break;
 
         for (auto& bucket: buckets){
-            for (auto value: bucket){
-                int index = (value - min) / nPowI % nBuckets;
+            for (auto& piece: bucket){
+                int index = (piece.value - min) / nPowI % nBuckets;
 
-                temp[index].push_back(value);
+                temp[index].push_back(piece);
             }
         }
         buckets = temp;
         clearBuckets(temp);
     }
 
-    std::vector<int> result;
-    for (auto& b: buckets){
-        for (auto v: b){
-            result.push_back(v);
+    std::vector<Piece> result;
+    for (auto& bucket: buckets){
+        for (auto piece: bucket){
+            result.push_back(piece);
         }
     }
     return result;
